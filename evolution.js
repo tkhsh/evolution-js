21616719a8bc0662532fc1ee07dde3487a6c4882
tkhsh <y-takahashi@timedia.co.jp>
2013-04-30 13:17:15 +0900

    関数drawWorldを作成

--- evolution.js
+++ evolution.js
@@ -4,3 +4,13 @@ function skipDay() {
 function updateWorld() {
 	//一日分のシミュレーションを進める
 }
+function drawWorld() {
+	var cells = "";
+	for(var i = 0; i < 30; i++) {
+		for(var j = 0; j < 100; j++) {
+			cells = cells.concat(".");
+		}
+		cells = cells.concat("\n");
+	}
+	document.getElementById("world").innerHTML = cells;
+}
